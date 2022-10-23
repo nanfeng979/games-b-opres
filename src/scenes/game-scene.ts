473c0b4b7c31d37afa70fb41c3e38b6dadfe4eba
yuqingxiang <yuqingxiang@easydo.cn>
2022-10-23 11:01:58 +0800

    鼠标点击后角色向鼠标方向前进

--- src/scenes/game-scene.ts
+++ src/scenes/game-scene.ts
@@ -1,4 +1,7 @@
+let player:any = {};
+
 export class GameScene extends Phaser.Scene {
+    private rope!: Phaser.Physics.Arcade.StaticGroup;
 
     constructor() {
       super({
@@ -11,10 +14,12 @@ export class GameScene extends Phaser.Scene {
         this.load.image("background", images_url + "background.png") // 蓝色背景
         this.load.image("lawn", images_url + "lawn.png") // 草坪
         this.load.image("stone", images_url + "stone.png") // 石块
-        this.load.spritesheet("star", images_url + "star.png", {frameWidth: 100, frameHeight: 100})
+        this.load.spritesheet("star", images_url + "star.png", {frameWidth: 100, frameHeight: 100}) // 宝石
+        this.load.image("rope", images_url + "rope.png") // 绳子
     }
 
     create(): void {
+        let _this = this
         this.add.image(2, 0, "background").setScale(187, 2.7) // 蓝色背景
 
         // 添加草坪组
@@ -40,10 +45,56 @@ export class GameScene extends Phaser.Scene {
         // 添加宝石
         // this.add.sprite(50, 100, "star").setScale(0.5)
 
-        let player = this.physics.add.sprite(375 / 2, 100, "player").setScale(0.6) // 人物
-        player.setBounce(0.2); // 弹力系数
+		// 人物
+        player = this.physics.add.sprite(375 / 2, 500, "player").setScale(0.5)
+        player.setBounce(0.3); // 弹力系数
         player.setCollideWorldBounds(true); // 与世界边框碰撞就停止
-        this.physics.add.collider(player, lawns);
+        this.physics.add.collider(player, lawns, pengzhuang);
+
+        // rope = this.add.image(50, 50, "rope").setAngle(90)
+        // this.rope = this.add.group({
+        //     key: "rope",
+        //     repeat: 5,
+        //     setXY: {x: 150, y: 300, stepX: 30}
+        // })
+
+        // this.rope = this.physics.add.staticGroup({
+        //     key: "rope",
+        //     // frame: ["blue1", "red1", "green1", "yellow1", "silver1", "purple1"],
+        //     frameQuantity: 2,
+        //     gridAlign: {
+        //       width: 10,
+        //       height: 6,
+        //       cellWidth: 32,
+        //       cellHeight: 32,
+        //       x: 112,//why?
+        //       y: 100,
+        //     },
+        //   })
+        
+
+        // 事件
+        this.input.on("pointerdown", (pointer:any) => {
+			let speed = 1
+			let x = pointer.x - player.x
+			let y = pointer.y - player.y
+			if(y > -130) y = -130
+
+			player.setVelocityX(x * speed)
+			player.setVelocityY(y * speed)
+		})
+
+		// 自定义函数
+		function pengzhuang() {
+			player.setVelocityX(0)
+		}
+        
+    }
+    
+    update(time: number, delta: number): void {
+        // rope.x = player.x;
+        // rope.y = player.y;
+        
     }
 
 }
